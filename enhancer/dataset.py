964f299f3a05416f3ab69e993a0cd2cd03830232
Hayden-Butler <HABHorsham@outlook.com>
2026-05-12 02:12:01 +0100

    datasest insalled

---
+++ enhancer/dataset.py
@@ -0,0 +1,27 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+from PIL import Image
+
+
+class SRDataset(Dataset):
+    def __init__(self, base_dataset, scale=4):
+        self.base = base_dataset
+        self.scale = scale
+        self.to_tensor = transforms.ToTensor()
+    
+    def __len__(self):
+        return len(self.base)
+    
+    def __getitem__(self, idx):
+        hr_image, _ = self.base[idx]
+
+        hr_width, hr_height = hr_image.size
+        lr_width, lr_height = hr_width//self.scale, hr_height//self.scale
+
+        lr_image = hr_image.resize((lr_width, lr_height), Image.Resampling.BICUBIC)
+
+        hr_tensor = self.to_tensor(hr_image)
+        lr_tensor = self.to_tensor(lr_image)
+
+        return lr_tensor, hr_tensor
